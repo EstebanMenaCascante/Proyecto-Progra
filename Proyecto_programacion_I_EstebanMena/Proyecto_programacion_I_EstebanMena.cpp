8bc304a5dc8a77c2ae4dd0aec196427f442c0ae9
EstebanMenaCascante <esteban.mena.cascante@est.una.ac.cr>
2024-09-03 16:02:58 -0600

    Se empezo la creacion del menu

--- Proyecto_programacion_I_EstebanMena/Proyecto_programacion_I_EstebanMena.cpp
+++ Proyecto_programacion_I_EstebanMena/Proyecto_programacion_I_EstebanMena.cpp
@@ -3,34 +3,50 @@
 
 #include <iostream>
 #include <stdio.h>
+#include "menu.h"
 
 using namespace std;
 
 int main()
 {
-	int opcion = 0;
+	int opcion;
 
+	printf("Bienvenido a Nueva Cinema\n\n");
+	printf("Que opcion desea escoger\n");
+	printf("Digite 1 para leer acerca de nuestro Cinema:\n");
+	printf("Digite 2 para acceder a mantenimiento:\n");
+	printf("Digite 3 para acceder a la reserva de asientos:\n");
+	printf("Digite 4 para acceder a la compra de asientos:\n");
+	printf("Digite 5 para cerrar el programa:\n\n");
+	printf("Digite la opcion que desea escoger: ");
+	scanf_s("%d", &opcion);
+
+	do
+	{
 	switch (opcion)
 	{
 	case 1:
-
+		printf("Digite 1 para leer acerca de nuestro Cinema:\n");
 
 
 		break;
 	case 2:
 
-
+		printf("Digite 4 para leer acerca de nuestro Cinema:\n");
 
 		break;
 	case 3:
 
-
+		printf("Digite 6 para leer acerca de nuestro Cinema:\n");
 
 		break;
 	default:
 		break;
 	}
-
+	
+	
+	
+	} while (opcion == 5);
     
 }
 
